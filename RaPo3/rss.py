04c147601caef607efb7f5617cc86bbc4e040fce
rapospectre <rapospectre@163.com>
2016-06-07 16:45:37 +0800

    add rss

---
+++ RaPo3/rss.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed
+
+from api.models import Article
+
+
+class ExtendedRSSFeed(Rss201rev2Feed):
+    mime_type = 'application/xml'
+    """
+    Create a type of RSS feed that has content:encoded elements.
+    """
+
+    def root_attributes(self):
+        attrs = super(ExtendedRSSFeed, self).root_attributes()
+        attrs['xmlns:content'] = 'http://purl.org/rss/1.0/modules/content/'
+        return attrs
+
+    def add_item_elements(self, handler, item):
+        super(ExtendedRSSFeed, self).add_item_elements(handler, item)
+        handler.addQuickElement(u'content:encoded', 'utf-8')
+
+
+class ArticleFeed(Feed):
+    feed_type = ExtendedRSSFeed
+    title = u"RaPoSpectre 的技术博客"
+    link = ""
+    description = "关注 RaPoSpectre 的最新动态"
+
+    def items(self):
+        return Article.objects.order_by('-create_time')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.title
+
+    def item_link(self, item):
+        return '/blog/{0}'.format(item.id)
--- RaPo3/urls.py
+++ RaPo3/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from RaPo3 import settings
+from RaPo3.rss import ArticleFeed
 from RaPo3.sitemaps import ArticleSitemap, StaticPageSitemap
 
 sitemaps = {
@@ -14,5 +15,6 @@ urlpatterns = patterns('',
                        url(r'^api/v1/', include('api.urls')),
                        url(r'^admin/api/', include('myadmin.urls')),
                        url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+                       url(r'^rss.xml$', ArticleFeed()),
                        url(r'^s/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_MEDIA}),
                        )
